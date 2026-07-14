15b364328f8225ce8f68cb8dd6ad5a58271ff705
Sidharth A <sid469369@gmail.com>
2026-07-14 19:46:47 +0530

    Phase 3 - Add service-to-service communication

--- order-service/config.py
+++ order-service/config.py
@@ -4,7 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SERVICE_NAME = "order-service"
+    SERVICE_NAME = os.getenv("SERVICE_NAME", "order-service")
     HOST = os.getenv("HOST","0.0.0.0")
     PORT = int(os.getenv("PORT",5003))
     DEBUG = os.getenv("DEBUG","True").lower() == "true"
+    USER_SERVICE_URL = os.getenv(
+        "USER_SERVICE_URL",
+        "http://user-service:5001"
+    )
+    PRODUCT_SERVICE_URL = os.getenv(
+        "PRODUCT_SERVICE_URL",
+        "http://product-service:5001"
+    )
--- order-service/services/order_services.py
+++ order-service/services/order_services.py
@@ -1,14 +1,38 @@
+import requests
+from config import Config
 from utils.logger import logger
-class OrderService:
 
+class OrderService:
     @staticmethod
     def get_all_orders():
+        
         logger.info("Fetching all orders")
-        return [
-            {
-                "id": 101,
-                "user_id": 1,
-                "product_id": 2,
-                "status": "Delivered"
+        
+        try:
+            user_response= requests.get(
+                f"{Config.USER_SERVICE_URL}/api/v1/users",
+                timeout=5
+            )
+            product_response= requests.get(
+                f"{Config.PRODUCT_SERVICE_URL}/api/v1/products",
+                timeout=5
+            )        
+        
+            return {
+                "orders": [
+                    {
+                        "id": 101,
+                        "user_id": 1,
+                        "product_id": 2,
+                        "status": "Delivered"
+                    }
+                ],
+                "users": user_response.json()["data"],
+                "products": product_response.json()["data"]
             }
-        ]
+
+        except requests.exceptions.RequestException as e:
+            logger.error(str(e))
+            return{
+                "error": "Unable to communicate with other services"
+            }
