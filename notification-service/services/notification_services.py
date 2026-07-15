7302b5f24c7b126d29ffc91310118768263ef1bb
Sidharth A <sid469369@gmail.com>
2026-07-15 20:18:22 +0530

    Integrate PostgreSQL with all microservices

--- notification-service/services/notification_services.py
+++ notification-service/services/notification_services.py
@@ -1,13 +1,35 @@
+from database.db import get_connection
 from utils.logger import logger
+
+
 class NotificationService:
 
     @staticmethod
     def get_all_notifications():
-        logger.info("Fetching all notifications")
+
+        logger.info("Fetching all notifications from PostgreSQL")
+
+        conn = get_connection()
+
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            SELECT id, user_id, message
+            FROM notifications
+            """
+        )
+
+        rows = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
         return [
             {
-                "id": 1,
-                "type": "Email",
-                "message": "Order shipped"
+                "id": row[0],
+                "user_id": row[1],
+                "message": row[2]
             }
+            for row in rows
         ]
